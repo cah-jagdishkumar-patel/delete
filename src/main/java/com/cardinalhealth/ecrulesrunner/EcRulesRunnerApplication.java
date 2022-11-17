d896da22f2330ae078ffed3829d227157ea886b4
Jagdish Patel <jagdishkumar.patel@cardinalhealth.com>
2022-11-17 12:43:45 -0500

    Initial commit

---
+++ src/main/java/com/cardinalhealth/ecrulesrunner/EcRulesRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.cardinalhealth.ecrulesrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcRulesRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcRulesRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardinalhealth/ecrulesrunner/EcRulesRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardinalhealth.ecrulesrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcRulesRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
